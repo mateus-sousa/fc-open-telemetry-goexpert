2c6ce9c52daa91e226bc865fed53accb307566d6
Mateus Silva <mateussilva@ntopus.com.br>
2024-04-29 12:05:17 -0300

    init request in service A

--- servico_a/main.go
+++ servico_a/main.go
@@ -16,37 +16,64 @@ type CEP struct {
 
 func main() {
 	r := chi.NewRouter()
-	r.Post("/users", Xpto)
+	r.Post("/getweather", getWeather)
 	http.ListenAndServe(":8000", r)
 }
 
-func Xpto(w http.ResponseWriter, r *http.Request) {
+func getWeather(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
+		return
 	}
 	var cep CEP
 	err = json.Unmarshal(body, &cep)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
+		return
 	}
 	err = validateCEP(cep)
 	if err != nil {
 		w.WriteHeader(http.StatusUnprocessableEntity)
-		w.Write([]byte("{'message': 'invalid zipcode'}"))
+		w.Write([]byte("invalid zipcode"))
+		return
 	}
-	req, err := http.NewRequestWithContext(context.Background(), "POST", "localhost:8081", bytes.NewBuffer(body))
+	req, err := http.NewRequestWithContext(context.Background(), "POST", "localhost:8081/getweather", bytes.NewBuffer(body))
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
 	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
 	}
-	if res.StatusCode != http.StatusOK {
-		panic(err)
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	handleResponse(w, res.StatusCode, resBody)
+}
+
+func handleResponse(w http.ResponseWriter, statusCode int, resBody []byte) {
+	if statusCode == http.StatusOK {
+		w.WriteHeader(http.StatusOK)
+		w.Write(resBody)
+		return
+	} else if statusCode == http.StatusUnprocessableEntity {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("invalid zipcode"))
+		return
+	} else if statusCode == http.StatusNotFound {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("can not find zipcode"))
+		return
 	}
 }
 
